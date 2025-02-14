069cf90fd0d2dfffec709ea0a61461500ed8d17c
T. Andrew Manning <manninga@illinois.edu>
2025-02-14 14:48:40 -0600

    Hard-code default data file server URL
    but env var DR_SERVER_URL can still override at runtime

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
-if [[ -z "$DR_SERVER_URL" ]]; then
-    DR_SERVER_URL=desdr-server.ncsa.illinois.edu
+# If a data file server URL is a non-empty string that is not the default value, search and replace.
+if [[ "x${DR_SERVER_URL}" != "x" && "$DR_SERVER_URL" != "desdr-server.ncsa.illinois.edu" ]]; then
+    find static/des_components -type f -exec sed -i "s/desdr-server.ncsa.illinois.edu/${DR_SERVER_URL}/g" {} \;
 fi
 
 set -euo pipefail
-
-find static/des_components -type f -exec sed -i "s/{{drserverbaseurl}}/${DR_SERVER_URL}/g" {} \;
 
 python main.py
